379024e7410fb19418568985ba5b63702f04abf3
Nikita <madeev@itglobal.ru>
2020-10-08 20:09:04 +0300

    update deps

--- rollup.config.js
+++ rollup.config.js
@@ -44,7 +44,7 @@ export default [
                 dev: !production,
                 // we'll extract any component CSS out into
                 // a separate file - better for performance
-                css: css => {
+                css: (css) => {
                     css.write("options.css");
                 },
                 preprocess: sveltePreprocess(),
