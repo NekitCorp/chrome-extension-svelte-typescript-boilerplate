6ee05b58eb557383ebb11f49350cd58a57570ce3
Nikita <warhammer-ss@yandex.ru>
2020-10-09 10:27:53 +0300

    update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,32 +1,35 @@
 import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
+import del from "rollup-plugin-delete";
 import svelte from "rollup-plugin-svelte";
 import { terser } from "rollup-plugin-terser";
 import sveltePreprocess from "svelte-preprocess";
 
 const production = !process.env.ROLLUP_WATCH;
 
-function svelteConfig(filename) {
+function createConfig(filename, { sveltePlugin, clear }) {
     return {
         input: `src/${filename}.ts`,
         output: {
-            sourcemap: true,
+            sourcemap: !production,
             format: "iife",
-            name: "app",
             file: `public/build/${filename}.js`,
         },
         plugins: [
-            svelte({
-                // enable run-time checks when not in production
-                dev: !production,
-                // we'll extract any component CSS out into
-                // a separate file - better for performance
-                css: (css) => {
-                    css.write(`${filename}.css`);
-                },
-                preprocess: sveltePreprocess(),
-            }),
+            clear && del({ targets: "public/build/*" }),
+
+            sveltePlugin &&
+                svelte({
+                    // enable run-time checks when not in production
+                    dev: !production,
+                    // we'll extract any component CSS out into
+                    // a separate file - better for performance
+                    css: css => {
+                        css.write(`${filename}.css`, !production);
+                    },
+                    preprocess: sveltePreprocess(),
+                }),
 
             // If you have external dependencies installed from
             // npm, you'll most likely need these plugins. In
@@ -53,26 +56,9 @@ function svelteConfig(filename) {
     };
 }
 
-function commonConfig(filename) {
-    return {
-        input: `src/${filename}.ts`,
-        output: {
-            sourcemap: true,
-            format: "iife",
-            file: `public/build/${filename}.js`,
-        },
-        plugins: [
-            typescript({
-                sourceMap: !production,
-                inlineSources: !production,
-            }),
-        ],
-    };
-}
-
 export default [
-    svelteConfig("options"),
-    svelteConfig("popup"),
-    commonConfig("background"),
-    commonConfig("content_script"),
+    createConfig("options", { sveltePlugin: true, clear: true }),
+    createConfig("popup", { sveltePlugin: true, clear: false }),
+    createConfig("background", { sveltePlugin: false, clear: false }),
+    createConfig("content_script", { sveltePlugin: false, clear: false }),
 ];
