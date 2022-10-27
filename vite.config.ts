563f810ca87929fde9454289671ff7e953530742
Nikita Madeev <n.madeev@softpro.com>
2022-10-27 16:23:50 +0300

    Fix HMR

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ const srcDir = resolve(__dirname, "src");
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [crx({ manifest }), svelte()],
+    plugins: [svelte(), crx({ manifest })],
     resolve: {
         alias: {
             src: srcDir,
