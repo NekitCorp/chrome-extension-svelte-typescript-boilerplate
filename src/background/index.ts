f89e8d573f154db27b4cabf8e9ed9c7282726205
idearat <idearat@coderats.llc>
2023-08-21 08:41:27 -0400

    Add full side_panel demo support.

--- src/background/index.ts
+++ src/background/index.ts
@@ -3,3 +3,8 @@ import { storage } from "../storage";
 chrome.runtime.onInstalled.addListener(() => {
     storage.get().then(console.log);
 });
+
+// Allows users to open the side panel by clicking on the action toolbar icon
+chrome.sidePanel.setPanelBehavior({ openPanelOnActionClick: true })
+    .catch((error) => console.error(error));
+
