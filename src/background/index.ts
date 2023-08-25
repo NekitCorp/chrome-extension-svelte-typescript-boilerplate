9506b4d945bc0fccdfb522e31357f0e13fad4137
idearat <idearat@coderats.llc>
2023-08-24 20:31:12 -0400

    Make side panel operation a commented option.

--- src/background/index.ts
+++ src/background/index.ts
@@ -4,7 +4,7 @@ chrome.runtime.onInstalled.addListener(() => {
     storage.get().then(console.log);
 });
 
-// Allows users to open the side panel by clicking on the action toolbar icon
-chrome.sidePanel.setPanelBehavior({ openPanelOnActionClick: true })
-    .catch((error) => console.error(error));
-
+// NOTE: If you want to toggle the side panel from the extension's action button,
+// you can use the following code:
+// chrome.sidePanel.setPanelBehavior({ openPanelOnActionClick: true })
+//    .catch((error) => console.error(error));
