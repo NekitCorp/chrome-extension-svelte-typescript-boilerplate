299f691fbc421845835a2765e29a6dcf69b532b1
Saba <sabato.sabato@gmail.com>
2024-09-11 21:34:48 +0300

    Remove debug messages - cleaner code

--- src/storage.ts
+++ src/storage.ts
@@ -11,35 +11,25 @@ export function persistentStore<T>(key: string, initialValue: T): Writable<T> {
     const store = writable(initialValue);
     let isUpdatingFromChrome = false;
     let isUpdatingFromStore = false;
-    const debug = true;
 
     function watchStore() {
-        if (debug) console.log("STORE subscribe");
         store.subscribe((value) => {
-            if (debug) console.log(`[${String(!isUpdatingFromChrome).toUpperCase()}] svelte => chrome ${value}`);
             // Prevent circular updates
             if (isUpdatingFromChrome) return;
-
             isUpdatingFromStore = true;
             chrome.storage.sync.set({ [key]: value }).then(() => {
                 isUpdatingFromStore = false;
             });
-            if (debug) console.log(`[END]  svelte => chrome ${value}`);
         });
     }
 
     function watchChrome() {
-        if (debug) console.log("CHROME subscribe");
         chrome.storage.onChanged.addListener((changes, namespace) => {
-            if (debug) console.log(`[${String(!isUpdatingFromStore).toUpperCase()}] chrome => svelte ${changes[key].newValue}`);
-
             // Prevent circular updates
             if (isUpdatingFromStore || namespace !== 'sync' || !(Object.hasOwn(changes, key))) return;
             isUpdatingFromChrome = true;
             store.set(changes[key].newValue);
             isUpdatingFromChrome = false;
-
-            if (debug) console.log(`[END]  chrome => svelte ${changes[key].newValue}`);
         });
     }
 
@@ -49,11 +39,9 @@ export function persistentStore<T>(key: string, initialValue: T): Writable<T> {
         if (!(Object.hasOwn(result, key))) {
             console.log(`Persistent store: couldn't find key [${key}] in chrome storage. Default to initial value [${initialValue}]`)
         }
-        if (debug) console.log(`[START] storage.sync.get => ${Object.hasOwn(result, key)}: ${value}`);
         store.set(value);
         watchStore();
         watchChrome();
-        if (debug) console.log(`[END]   storage.sync.get => ${Object.hasOwn(result, key)}: ${value}`);
     });
 
     return store;
